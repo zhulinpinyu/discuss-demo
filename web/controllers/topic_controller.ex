510896684a1e97a05f6b1491deb094bcee9d37c9
zhulinpinyu <mulixiang199016@gmail.com>
2016-12-09 00:11:20 +0800

    add form （only code）un work

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,11 +1,11 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   def new(conn,params) do
-    IO.puts "++++++"
-    IO.inspect conn
-    IO.puts "++++++"
-    IO.inspect params
-    IO.puts "++++++"
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
   end
 end
