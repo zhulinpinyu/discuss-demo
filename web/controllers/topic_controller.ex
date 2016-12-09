6192d54f31e8b43fe9d88e4cb6a625e1ce3b68e8
zhulinpinyu <mulixiang199016@gmail.com>
2016-12-09 09:18:57 +0800

    handle form submit

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,9 +3,16 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
-  def new(conn,params) do
+  def new(conn, params) do
     changeset = Topic.changeset(%Topic{}, %{})
-
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, params) do
+    IO.puts "++++++++++++++++++++++++++++++++++++"
+    IO.inspect params["topic"]
+    %{"topic" => topic} = params
+    IO.inspect topic
+    IO.puts "++++++++++++++++++++++++++++++++++++"
   end
 end
