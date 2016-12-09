3674b24e5b0623147d1195d5883c4f83055f217b
zhulinpinyu <mulixiang199016@gmail.com>
2016-12-09 10:42:39 +0800

    clear code

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,15 +3,13 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
   end
 
-  def create(conn, params) do
+  def create(conn, %{"topic" => topic}) do
     IO.puts "++++++++++++++++++++++++++++++++++++"
-    IO.inspect params["topic"]
-    %{"topic" => topic} = params
     IO.inspect topic
     IO.puts "++++++++++++++++++++++++++++++++++++"
   end
