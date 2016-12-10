b6006ec6c695226f9ecb6bc6deb3971f6b9d8fbd
zhulinpinyu <mulixiang199016@gmail.com>
2016-12-10 16:48:59 +0800

    github oauth worked

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,7 +2,34 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+  alias Discuss.User
 
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    user_params = %{email: auth.info.email, token: auth.credentials.token, provider: "github"}
+    User.changeset(%User{}, user_params)
+    |> signin(conn)
+  end
+
+  defp signin(changeset,conn) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome Back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in.")
+        |> redirect(to: topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
