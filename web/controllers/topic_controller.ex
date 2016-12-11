32c2f66a1738d430924357ce6a79fce6c9d1bfe9
zhulinpinyu <mulixiang199016@gmail.com>
2016-12-12 00:13:19 +0800

    custon Plug function for correct access permission

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,6 +4,7 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:edit, :update, :delete]
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
@@ -56,5 +57,16 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Delete Topic")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{ params: %{ "id" => topic_id } } = conn
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "Warnning,Permission Denied!")
+      |> redirect(to: topic_path(conn, :index))
+    end
   end
 end
